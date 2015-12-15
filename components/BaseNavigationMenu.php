4f3169306d4a9c0b2a83797ff2f6dbd57993e2ab
Carl Mai <carl.schoenbach@gmail.com>
2015-12-15 22:50:51 +0100

    add a isitemactive method to BaseNavigationMenu

--- components/BaseNavigationMenu.php
+++ components/BaseNavigationMenu.php
@@ -130,4 +130,25 @@ class BaseNavigationMenu
 		}
 		return false;
 	}
+
+
+	/* copied from CMenu **/
+	function isItemActive($item)
+    {  
+		$route=Yii::app()->getController()->getRoute();
+        if(isset($item['url']) && is_array($item['url']) && !strcasecmp(trim($item['url'][0],'/'),$route))
+        {
+            unset($item['url']['#']);
+            if(count($item['url'])>1)
+            {
+                foreach(array_splice($item['url'],1) as $name=>$value)
+                {
+                    if(!isset($_GET[$name]) || $_GET[$name]!=$value)
+                        return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }  
 }
